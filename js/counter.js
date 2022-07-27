834a0d25634af8122a27c335160fd96293e72fc7
edo979 <prezentabilan@gmail.com>
2022-07-27 17:30:34 +0200

    add js file

---
+++ js/counter.js
@@ -0,0 +1,48 @@
+class Counter {
+  constructor(minutes = 0, seconds = 0) {
+    this.secondsEl = document.querySelector('[data-timer="seconds"]')
+
+    this.seconds = seconds
+    this.minutes = minutes
+  }
+
+  toggleFlipClass() {
+    this.secondsEl.classList.toggle('flip')
+  }
+
+  changeTime() {
+    this.seconds--
+    this.isTimeUnitFromBegining('seconds')
+    //console.log(this.seconds, this.minutes)
+  }
+
+  isTimeUnitFromBegining(timeUnit) {
+    switch (timeUnit) {
+      case 'seconds':
+        if (this.seconds < 0) {
+          this.seconds = 59
+          this.minutes--
+          this.isTimeUnitFromBegining('minutes')
+        }
+        break
+
+      case 'minutes':
+        if (this.minutes < 0) {
+          this.minutes = 59
+        }
+        break
+
+      default:
+        break
+    }
+  }
+
+  tick() {
+    setInterval(() => {
+      this.changeTime()
+      //this.toggleFlipClass()
+    }, 1000)
+  }
+}
+
+export const counter = new Counter()
