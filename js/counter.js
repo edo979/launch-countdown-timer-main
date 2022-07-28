badae09a308be96de71f816a9c03cd055d8bc87d
edo979 <prezentabilan@gmail.com>
2022-07-28 19:23:14 +0200

    working on js

--- js/counter.js
+++ js/counter.js
@@ -6,8 +6,29 @@ class Counter {
     this.minutes = minutes
   }
 
-  toggleFlipClass() {
-    this.secondsEl.classList.toggle('flip')
+  flipCard() {
+    // set number in top background card
+    const topFillNumberEl = document.getElementById('flip-fill-top')
+    topFillNumberEl.textContent = this.seconds
+
+    // set flip class to inner flip-card
+    this.secondsEl.querySelector('.flip-card_inner').classList.toggle('flip')
+
+    // after flip set bottom card number
+    const bottomNumberEl = document.getElementById('flip-bottom')
+    bottomNumberEl.textContent = this.seconds
+
+    // reset cards get ready for next flip, set numbers
+    const topFrontNumberEl = document.getElementById('flip-front')
+    topFrontNumberEl.textContent = this.seconds
+
+    setTimeout(
+      () =>
+        this.secondsEl
+          .querySelector('.flip-card_inner')
+          .classList.toggle('flip'),
+      500
+    )
   }
 
   changeTime() {
@@ -38,10 +59,10 @@ class Counter {
   }
 
   tick() {
-    // setInterval(() => {
-    //   this.changeTime()
-    //   //this.toggleFlipClass()
-    // }, 1000)
+    setInterval(() => {
+      this.changeTime()
+      this.flipCard()
+    }, 1000)
   }
 }
 
