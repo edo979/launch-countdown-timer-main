7568185fb5ffcd92cb73840a7d159a1217b39b32
edo979 <prezentabilan@gmail.com>
2022-07-31 08:54:19 +0200

    count to custom date

--- js/counter.js
+++ js/counter.js
@@ -170,6 +170,35 @@ class Counter {
     this.countdownTimeEl.querySelector('#seconds').textContent =
       'seconds ' + this.seconds
   }
+
+  /**
+   * @param {Date} dateFuture
+   */
+  countToTimeDate(dateFuture) {
+    // https://stackoverflow.com/questions/13903897/javascript-return-number-of-days-hours-minutes-seconds-between-two-dates
+    // get total seconds between the times
+    let delta = Math.abs(dateFuture - new Date()) / 1000
+
+    // calculate (and subtract) whole days
+    const days = Math.floor(delta / 86400)
+    delta -= days * 86400
+
+    // calculate (and subtract) whole hours
+    const hours = Math.floor(delta / 3600) % 24
+    delta -= hours * 3600
+
+    // calculate (and subtract) whole minutes
+    const minutes = Math.floor(delta / 60) % 60
+    delta -= minutes * 60
+
+    // what's left is seconds
+    const seconds = Math.floor(delta % 60) // in theory the modulus is not required
+
+    this.days = days
+    this.hours = hours
+    this.minutes = minutes
+    this.seconds = seconds
+  }
 }
 
 export const counter = new Counter()
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './scss/style.scss'
 import { counter } from './js/counter'
 
+counter.countToTimeDate(new Date('2022-08-15'))
 counter.start()
